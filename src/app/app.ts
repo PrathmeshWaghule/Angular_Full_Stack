70905e413d8e24952a69d605950f23507e0023bd
Prathmesh Ashok Waghule <143677829+PrathmeshWaghule@users.noreply.github.com>
2025-10-26 23:06:37 +0530

    Event-Binding
    > this Binding mechanism is used to bind HTML -> TS
    > to achieve Event - Binding we use circular () braces

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrathmeshComponent } from './prathmesh-component/prathmesh-component';
-import { PythonComponent } from './python-component/python-component';
-import { LBComponent } from './lbcomponent/lbcomponent';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,PrathmeshComponent,PythonComponent,LBComponent],
+  imports: [RouterOutlet,PrathmeshComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Inline');
+  protected readonly title = signal('EventBinding');
 }
--- src/app/prathmesh-component/prathmesh-component.ts
+++ src/app/prathmesh-component/prathmesh-component.ts
@@ -6,6 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './prathmesh-component.html',
   styleUrl: './prathmesh-component.css',
 })
-export class PrathmeshComponent {
+export class PrathmeshComponent 
+{
+  public Information : string = " ";
 
+  public ButtonEven()
+  {
+    console.log("Click me Button Pressed..!");
+  }
+
+  public AngularEvent()
+  {
+    this.Information = "Angular Button Pressed..!";
+  }
+
+  public PythonEvent(Value : any)
+  {
+    console.log(Value);
+  }
 }
